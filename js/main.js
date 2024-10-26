ad85403f82262539080b05e7c523d347a95c9048
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-10-26 16:46:55 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ window.onload = function () {
     const photo = user.photo_url;
     // const theme = window.Telegram.WebApp.initDataUnsafe.colorScheme;
     // const color = window.Telegram.WebApp.initDataUnsafe.themeParams;
+    console.log("вывод содержимого в обьекте Userэ");
     console.log(`вывод содержимого в обьекте User - ${user}`);
     console.log(`что получаю в user.is_premium  ${user.is_premium}`);
     console.log(`что получаю в user.colorScheme  ${user.colorScheme}`);
