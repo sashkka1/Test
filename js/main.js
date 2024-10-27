acbc155f2d4e92cd9758bd9a004b13645f15b80d
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-10-27 18:36:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,18 +23,17 @@ function how_tma(){
     // window.Telegram.WebApp.CloudStorage.setItem("Name", Name);
     // Name ="asdasdasdasd";
     window.Telegram.WebApp.CloudStorage.getItem("Name", (err, NewName) => {
-        if(NewName !== undefined){
-            console.log(`вывод 1  ${NewName}`);
-            Name = NewName + NewName;
-            console.log(`вывод 2  ${Name}`);
-        }else{
-            Name ="asdf";
-            console.log(`вывод 3  ${Name}`);
-        }
-        console.log(`вывод успешн ${Name}`);
     });
     document.getElementById('notification-count').innerHTML = `Имя ${Name}`;
-    console.log(`вывод 4  ${Name}`);
+    console.log(`вывод 1  ${Name}`);
+    if(NewName !== ''){
+        Name ="asdf";
+        console.log(`вывод 2  ${Name}`);
+    }else{
+        console.log(`вывод 3  ${NewName}`);
+        Name = NewName + NewName;
+        console.log(`вывод 4  ${Name}`);
+    }
     window.Telegram.WebApp.CloudStorage.setItem("Name", Name);
     // window.Telegram.WebApp.CloudStorage.removeItem("count")
     // count = count + 1;
@@ -59,7 +58,7 @@ window.onload = function () {
     const user = window.Telegram.WebApp.initDataUnsafe.user;
 
 
-    document.getElementById('notificationp').innerHTML = "Test 8";
+    document.getElementById('notificationp').innerHTML = "Test 9";
 
     how_tma()
 
