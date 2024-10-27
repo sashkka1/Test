6b78ed79a0dc7358009cc3de080a5e5749f7e3ee
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-10-27 18:26:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,18 +18,23 @@ function Use_work() {
 function how_tma(){
     let Name;
     let NewName;
+    console.log(`вывод 0  ${Name}`);
     // Name ="asdf";
     // window.Telegram.WebApp.CloudStorage.setItem("Name", Name);
     // Name ="asdasdasdasd";
     window.Telegram.WebApp.CloudStorage.getItem("Name", (err, NewName) => {
         if(NewName !== undefined){
+            console.log(`вывод 1  ${NewName}`);
             Name = NewName + NewName;
+            console.log(`вывод 2  ${Name}`);
         }else{
             Name ="asdf";
+            console.log(`вывод 3  ${Name}`);
         }
         console.log(`вывод успешн ${Name}`);
     });
     document.getElementById('notification-count').innerHTML = `Имя ${Name}`;
+    console.log(`вывод 4  ${Name}`);
     window.Telegram.WebApp.CloudStorage.setItem("Name", Name);
     // window.Telegram.WebApp.CloudStorage.removeItem("count")
     // count = count + 1;
@@ -54,7 +59,7 @@ window.onload = function () {
     const user = window.Telegram.WebApp.initDataUnsafe.user;
 
 
-    document.getElementById('notificationp').innerHTML = "Test 7";
+    document.getElementById('notificationp').innerHTML = "Test 8";
 
     how_tma()
 
